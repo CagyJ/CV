cb3febf170c48bd16e3c9e737dbb2564f48a0cde
CagyJ <josh.jiao.322@gmail.com>
2020-01-23 13:58:49 +0100

    Image Basic Operation

--- CVFundamental/lowLevel/basicOperation.py
+++ CVFundamental/lowLevel/basicOperation.py
@@ -19,4 +19,36 @@ def resize_img(img, width):
     dimension = (int(width), int(img.shape[0]*ratio))
     return cv2.resize(img, dimension, interpolation = cv2.INTER_AREA)
 
+def img_cooler(img,b_increase,r_decrease):
+    B,G,R = cv2.split(img)
+    b_lim = 255 - b_increase 
+    B[B>b_lim] = 255
+    B[B<=b_lim] = (b_increase + B[B<=b_lim]).astype(img.dtype) 
+    
+    r_lim = r_decrease 
+    R[R<r_lim] = 0
+    R[R>r_lim] = (R[R>r_lim] - r_decrease).astype(img.dtype)
+    return cv2.merge((B,G,R))
+
+
+def img_warmer(img, r_increase, b_decrease):
+    B,G,R = cv2.split(img)
+    b_lim = b_decrease 
+    B[B<b_lim] = 0
+    B[B>b_lim] = (B[B>b_lim] - b_decrease).astype(img.dtype) 
+    
+    r_lim = r_increase 
+    R[R>r_lim] = 255
+    R[R<=r_lim] = (R[R<=r_lim] + r_increase).astype(img.dtype)
+    return cv2.merge((B,G,R))
+
+
+def adjust_gamma(img,gamma=1.0):
+    invGamma = 1.0/gamma
+    table = []
+    for i in range(256):
+        table.append(((i/255.0)**invGamma)*255) 
+    table = np.array(table).astype('uint8')
+    return cv2.LUT(img,table)
+
 

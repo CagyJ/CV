9bfac571279695072d98dd9b654f735b2bc1784a
CagyJ <josh.jiao.322@gmail.com>
2020-08-14 16:16:38 +0200

    simple linear regression2

--- ML/linear/simpleLinearRegression.py
+++ ML/linear/simpleLinearRegression.py
@@ -42,6 +42,46 @@ class SimpleLinearRegression1:
     def __repr__(self):
         return "SimpleLinearRegression1()"
 
+
+class SimpleLinearRegression2:
+
+    def __init__(self):
+        self.a_ = None
+        self.b_ = None
+    
+    def fit(self, x_train, y_train):
+        assert x_train.ndim == 1, \
+            "It should be single feature training data."
+        assert len(x_train) == len(y_train), \
+            "the length of feature training data should be equal to the target training data."
+        
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        num = (x_train - x_mean).dot(y_train - y_mean)
+        d = (x_train - x_mean).dot(x_train - x_mean)
+
+        
+        self.a_ = num/d 
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+    
+    def predict(self, x_predict):
+        assert x_predict.ndim == 1,\
+            "It should be single feature training data."
+        assert self.a_ is not None and self.b_ is not None, \
+            "must fit before predict!"
+        
+        return np.array([self._predict(x) for x in x_predict])
+    
+    def _predict(self, x_single):
+        return self.a_ * x_single + self.b_
+    
+    def __repr__(self):
+        return "SimpleLinearRegression2()"
+
+
 if __name__ == "__main__":
     x = np.array([1., 2., 3., 4., 5.])
     y = np.array([1., 3., 2., 3., 5.])
